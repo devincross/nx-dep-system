0fd0bc199659b48fc09e90a18c32032cf0b95ed5
Devin Eden <devin.ede@gabbwireless.com>
2026-03-04 15:02:51 -0700

    fix: add @org/database webpack alias for Docker builds

--- apps/client-api/webpack.config.js
+++ apps/client-api/webpack.config.js
@@ -9,6 +9,11 @@ module.exports = {
       devtoolModuleFilenameTemplate: '[absolute-resource-path]',
     }),
   },
+  resolve: {
+    alias: {
+      '@org/database': join(__dirname, '../../libs/database/src/index.ts'),
+    },
+  },
   plugins: [
     new NxAppWebpackPlugin({
       target: 'node',
